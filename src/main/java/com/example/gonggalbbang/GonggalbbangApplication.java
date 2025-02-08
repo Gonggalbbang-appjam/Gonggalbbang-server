6f886051560ad9b280601e7f0d4f7de82f83037e
Shuung <qmoy2510@gmail.com>
2025-02-08 17:26:41 +0900

    .

---
+++ src/main/java/com/example/gonggalbbang/GonggalbbangApplication.java
@@ -0,0 +1,13 @@
+package com.example.gonggalbbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonggalbbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonggalbbangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gonggalbbang/GonggalbbangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gonggalbbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonggalbbangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
