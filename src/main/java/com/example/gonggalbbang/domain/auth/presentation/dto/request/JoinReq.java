19de60cb04bec0ae928878ee3eddf65388c0f4b8
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-02-08 17:56:38 +0900

    .

--- src/main/java/com/example/gonggalbbang/domain/auth/presentation/dto/request/JoinReq.java
+++ src/main/java/com/example/gonggalbbang/domain/auth/presentation/dto/request/JoinReq.java
@@ -3,7 +3,7 @@ package com.example.gonggalbbang.domain.auth.presentation.dto.request;
 import com.example.gonggalbbang.domain.auth.domain.entity.User;
 import com.example.gonggalbbang.domain.auth.domain.enums.UserType;
 
-public record JoinReq(String username, String name, String password, int number) {
+public record JoinReq(String username, String name, String password) {
 
     public static User fromJoinReq(JoinReq joinReq, String password) {
         return User.builder()
